4da9b3edece58e383d481652e38447a048bbb599
nsc-ko-kurita <63280543+nsc-ko-kurita@users.noreply.github.com>
2020-04-21 09:45:44 +0900

    Add files via upload

--- kadai2/sosu.js
+++ kadai2/sosu.js
@@ -1,5 +1,19 @@
-function sosu(num){
-    if(num > 1){
-        
+function sosu (num) {
+    if(num == 2) {
+        return '素数です';
+    } else {
+        for(i = 2; i < num; i++) {
+            if(num % i == 0) {
+                return '素数ではない';
+                break;
+            }
+            if(i + 1 == num) {
+                return '素数です';
+            }
+        }
     }
 }
+
+
+console.log(sosu(6)) ;
+console.log(sosu(2));
